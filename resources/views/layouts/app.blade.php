2dff74b3f29c5c770dcf6fb9ffa928c673954526
Ali Ghasemzadeh <alighasemzadeh.dev@gmail.com>
2021-10-28 20:03:25 +0330

    Fix Layouts.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ config('bap.title', 'BAP') }} @if(isset($title)) - {{ $title }} @endif</title>
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
@@ -18,26 +18,187 @@
         <!-- Scripts -->
         <script src="{{ mix('js/app.js') }}" defer></script>
     </head>
-    <body class="font-sans antialiased">
-        <x-jet-banner />
-
-        <div class="min-h-screen bg-gray-100">
-            @livewire('navigation-menu')
-
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
+    <body class="font-sans antialiased bg-gray-50 text-base pt-14 lg:pl-60">
+    <div id="app" class="w-screen transition-all lg:w-auto">
+        <nav id="navbar-main" class="navbar is-fixed-top">
+            <div class="navbar-brand">
+                <a class="navbar-item mobile-aside-button">
+                    <span class="icon"><i class="mdi mdi-forwardburger mdi-24px"></i></span>
+                </a>
+                <div class="navbar-item">
+                    <div class="control"><input placeholder="Search everywhere..." class="input"></div>
+                </div>
+            </div>
+            <div class="navbar-brand is-right">
+                <a class="navbar-item --jb-navbar-menu-toggle" data-target="navbar-menu">
+                    <span class="icon"><i class="mdi mdi-dots-vertical mdi-24px"></i></span>
+                </a>
+            </div>
+            <div class="navbar-menu" id="navbar-menu">
+                <div class="navbar-end">
+                    <div class="navbar-item dropdown has-divider">
+                        <a class="navbar-link">
+                            <span class="icon"><i class="mdi mdi-menu"></i></span>
+                            <span>Sample Menu</span>
+                            <span class="icon">
+                            <i class="mdi mdi-chevron-down"></i>
+                          </span>
+                        </a>
+                        <div class="navbar-dropdown">
+                            <a href="profile.html" class="navbar-item">
+                                <span class="icon"><i class="mdi mdi-account"></i></span>
+                                <span>My Profile</span>
+                            </a>
+                            <a class="navbar-item">
+                                <span class="icon"><i class="mdi mdi-settings"></i></span>
+                                <span>Settings</span>
+                            </a>
+                            <a class="navbar-item">
+                                <span class="icon"><i class="mdi mdi-email"></i></span>
+                                <span>Messages</span>
+                            </a>
+                            <hr class="navbar-divider">
+                            <a class="navbar-item">
+                                <span class="icon"><i class="mdi mdi-logout"></i></span>
+                                <span>Log Out</span>
+                            </a>
+                        </div>
                     </div>
-                </header>
-            @endif
+                    <div class="navbar-item dropdown has-divider has-user-avatar">
+                        <a class="navbar-link">
+                            <div class="user-avatar">
+                                <img src="https://avatars.dicebear.com/v2/initials/john-doe.svg" alt="John Doe" class="rounded-full">
+                            </div>
+                            <div class="is-user-name"><span>John Doe</span></div>
+                            <span class="icon"><i class="mdi mdi-chevron-down"></i></span>
+                        </a>
+                        <div class="navbar-dropdown">
+                            <a href="profile.html" class="navbar-item">
+                                <span class="icon"><i class="mdi mdi-account"></i></span>
+                                <span>My Profile</span>
+                            </a>
+                            <a class="navbar-item">
+                                <span class="icon"><i class="mdi mdi-settings"></i></span>
+                                <span>Settings</span>
+                            </a>
+                            <a class="navbar-item">
+                                <span class="icon"><i class="mdi mdi-email"></i></span>
+                                <span>Messages</span>
+                            </a>
+                            <hr class="navbar-divider">
+                            <a class="navbar-item">
+                                <span class="icon"><i class="mdi mdi-logout"></i></span>
+                                <span>Log Out</span>
+                            </a>
+                        </div>
+                    </div>
+                    <a href="https://justboil.me/tailwind-admin-templates" class="navbar-item has-divider desktop-icon-only">
+                        <span class="icon"><i class="mdi mdi-help-circle-outline"></i></span>
+                        <span>About</span>
+                    </a>
+                    <a href="https://github.com/justboil/admin-one-tailwind" class="navbar-item has-divider desktop-icon-only">
+                        <span class="icon"><i class="mdi mdi-github-circle"></i></span>
+                        <span>GitHub</span>
+                    </a>
+                    <a title="Log out" class="navbar-item desktop-icon-only">
+                        <span class="icon"><i class="mdi mdi-logout"></i></span>
+                        <span>Log out</span>
+                    </a>
+                </div>
+            </div>
+        </nav>
+        <aside class="aside is-placed-left is-expanded">
+            <div class="aside-tools">
+                <div>
+                    {{ config('bap.title', 'BAP') }}
+                </div>
+            </div>
+            <div class="menu is-menu-main">
+                <p class="menu-label">General</p>
+                <ul class="menu-list">
+                    <li class="active">
+                        <a href="index.html">
+                            <span class="icon"><i class="mdi mdi-desktop-mac"></i></span>
+                            <span class="menu-item-label">Dashboard</span>
+                        </a>
+                    </li>
+                </ul>
+                <p class="menu-label">Examples</p>
+                <ul class="menu-list">
+                    <li class="--set-active-tables-html">
+                        <a href="tables.html">
+                            <span class="icon"><i class="mdi mdi-table"></i></span>
+                            <span class="menu-item-label">Tables</span>
+                        </a>
+                    </li>
+                    <li class="--set-active-forms-html">
+                        <a href="forms.html">
+                            <span class="icon"><i class="mdi mdi-square-edit-outline"></i></span>
+                            <span class="menu-item-label">Forms</span>
+                        </a>
+                    </li>
+                    <li class="--set-active-profile-html">
+                        <a href="profile.html">
+                            <span class="icon"><i class="mdi mdi-account-circle"></i></span>
+                            <span class="menu-item-label">Profile</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="login.html">
+                            <span class="icon"><i class="mdi mdi-lock"></i></span>
+                            <span class="menu-item-label">Login</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a class="dropdown">
+                            <span class="icon"><i class="mdi mdi-view-list"></i></span>
+                            <span class="menu-item-label">Submenus</span>
+                            <span class="icon"><i class="mdi mdi-plus"></i></span>
+                        </a>
+                        <ul>
+                            <li>
+                                <a href="#void">
+                                    <span>Sub-item One</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#void">
+                                    <span>Sub-item Two</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+                <p class="menu-label">About</p>
+                <ul class="menu-list">
+                    <li>
+                        <a href="https://justboil.me" onclick="alert('Coming soon'); return false" target="_blank" class="has-icon">
+                            <span class="icon"><i class="mdi mdi-credit-card-outline"></i></span>
+                            <span class="menu-item-label">Premium Demo</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://justboil.me/tailwind-admin-templates" class="has-icon">
+                            <span class="icon"><i class="mdi mdi-help-circle"></i></span>
+                            <span class="menu-item-label">About</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://github.com/justboil/admin-one-tailwind" class="has-icon">
+                            <span class="icon"><i class="mdi mdi-github-circle"></i></span>
+                            <span class="menu-item-label">GitHub</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </aside>
+        <section class="section main-section">
+            {{ $slot }}
+        </section>
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
-        </div>
+
+    </div>
+
 
         @stack('modals')
 
