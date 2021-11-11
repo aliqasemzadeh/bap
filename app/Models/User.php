4403194b00be0db60d5ecf494be186b9d0e47b8b
Ali Ghasemzadeh <alighasemzadeh.dev@gmail.com>
2021-11-11 16:34:32 +0330

    Add Auth Logs.

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,6 +9,7 @@ use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
 use Laravel\Jetstream\HasProfilePhoto;
 use Laravel\Sanctum\HasApiTokens;
+use Yadahan\AuthenticationLog\AuthenticationLogable;
 
 class User extends Authenticatable
 {
@@ -17,6 +18,7 @@ class User extends Authenticatable
     use HasProfilePhoto;
     use Notifiable;
     use TwoFactorAuthenticatable;
+    use AuthenticationLogable;
 
     /**
      * The attributes that are mass assignable.
---
+++ resources/lang/en/bap.php
@@ -0,0 +1,4 @@
+<?php
+return [
+    'home' => 'Home',
+];
