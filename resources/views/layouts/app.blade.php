ea62d32b396ce23242826558107ed2123e62fab1
Ali Ghasemzadeh <alighasemzadeh.dev@gmail.com>
2021-11-11 17:07:55 +0330

    Add Title.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ config('app.name', 'Laravel') }} @if(isset($title)) - {{ $title }} @endif</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:400,600,700" rel="stylesheet">
