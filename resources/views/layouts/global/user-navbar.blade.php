247343b573921a41441187fe6341df00e70784c4
Ali Ghasemzadeh <alighasemzadeh.dev@gmail.com>
2021-11-18 00:39:14 +0330

    Add Avatar

--- resources/views/layouts/global/user-navbar.blade.php
+++ resources/views/layouts/global/user-navbar.blade.php
@@ -17,9 +17,15 @@
     @endauth
     <div class="nav-item dropdown">
         <a href="#" class="nav-link d-flex lh-1 text-reset p-0" data-bs-toggle="dropdown" aria-label="Open user menu">
-            <span class="avatar avatar-sm">
-	            <img src="{{ auth()->user()->gravatar }}">
-            </span>
+
+            @auth
+                <span class="avatar avatar-sm" style="background-image: url({{ auth()->user()->gravatar }})"></span>
+            @endauth
+            @guest
+                <span class="avatar avatar-sm">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><circle cx="12" cy="7" r="4" /><path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" /></svg>
+                </span>
+            @endguest
         </a>
 
         <div class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
