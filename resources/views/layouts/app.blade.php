ffd028fc253fac101abef197ae4bb948d711ad45
erikn69 <erikn_69@hotmail.com>
2021-11-24 11:33:23 -0500

    Link to 'admin.dashboard.index' on main page
    Only if they can admin.dashboard.index

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -64,6 +64,18 @@
                                     </span>
                                 </a>
                             </li>
+                            @can('admin_dashboard_index')
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('admin.dashboard.index') }}" >
+                                    <span class="nav-link-icon d-md-none d-lg-inline-block"><!-- Download SVG icon from http://tabler-icons.io/i/home -->
+	                                    <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><circle cx="12" cy="13" r="2" /><line x1="13.45" y1="11.55" x2="15.5" y2="9.5" /><path d="M6.4 20a9 9 0 1 1 11.2 0z" /></svg>
+                                    </span>
+                                    <span class="nav-link-title">
+                                      {{ __('bap.permissions.admin_dashboard_index') }}
+                                    </span>
+                                </a>
+                            </li>
+                            @endcan
                             @endauth
                         </ul>
                     </div>
