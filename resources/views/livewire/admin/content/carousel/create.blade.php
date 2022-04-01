101d7063b5166a2d3fc83aef9de3571ebdb2d983
Ali Ghasemzadeh <alighasemzadeh.dev@gmail.com>
2022-04-01 22:57:53 +0430

    Slider working.

--- resources/views/livewire/admin/content/carousel/create.blade.php
+++ resources/views/livewire/admin/content/carousel/create.blade.php
@@ -1,3 +1,58 @@
-<div>
-    {{-- The Master doesn't talk, he acts. --}}
+<div class="modal-dialog modal-xl">
+    <form wire:submit.prevent="create">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">{{ __('bap.create_carousel') }}</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="{{ __('bap.close') }}"></button>
+            </div>
+            <div class="modal-body">
+                <div class="row">
+                    <div class="col-md-12">
+
+                        <div class="mb-3">
+                            <label class="form-label" for="title">{{ __('bap.title') }}</label>
+                            <input type="text" wire:model="title" class="form-control @error('title') is-invalid @enderror" name="title" placeholder="{{ __('bap.title') }}">
+                            @error('title')
+                            <div class="invalid-feedback">{{ $message }}</div>
+                            @enderror
+                        </div>
+
+                        <div class="mb-3">
+                            <div class="form-label">{{ __('bap.image') }}</div>
+                            <input type="file" wire:model="image" class="form-control @error('image') is-invalid @enderror" name="image">
+                            @error('image')
+                            <div class="invalid-feedback">{{ $message }}</div>
+                            @enderror
+                        </div>
+
+                        <div class="mb-3">
+                            <label class="form-label" for="type">{{ __('bap.language') }}</label>
+                            <select wire:model="language" class="form-control @error('language') is-invalid @enderror" name="language">
+                                <option></option>
+                                @foreach(config('laravellocalization.supportedLocales') as $key => $value)
+                                    <option value="{{ $key }}">{{ config('laravellocalization.supportedLocales.'.$key.'.name') }}</option>
+                                @endforeach
+                            </select>
+                            @error('language')
+                            <div class="invalid-feedback">{{ $message }}</div>
+                            @enderror
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label" for="description">{{ __('bap.description') }}</label>
+                            <textarea wire:model="description" class="form-control @error('description') is-invalid @enderror" name="description"></textarea>
+                            @error('description')
+                            <div class="invalid-feedback">{{ $message }}</div>
+                            @enderror
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">{{ __('bap.close') }}</button>
+                <button type="submit" class="btn btn-primary">{{ __('bap.create') }}</button>
+            </div>
+        </div>
+    </form>
 </div>
+
