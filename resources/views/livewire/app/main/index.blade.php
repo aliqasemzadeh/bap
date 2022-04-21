2e0c16099c5b5f227c73af1b511d74649ce99e4e
Ali Ghasemzadeh <alighasemzadeh.dev@gmail.com>
2022-04-21 12:00:26 +0430

    fix home-carousel controls.

--- resources/views/livewire/app/main/index.blade.php
+++ resources/views/livewire/app/main/index.blade.php
@@ -25,11 +25,11 @@
                             </div>
                             @endforeach
                         </div>
-                        <a class="carousel-control-prev" href="#carousel-captions" role="button" data-bs-slide="prev">
+                        <a class="carousel-control-prev" href="#home-carousel" role="button" data-bs-slide="prev">
                             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                             <span class="visually-hidden">Previous</span>
                         </a>
-                        <a class="carousel-control-next" href="#carousel-captions" role="button" data-bs-slide="next">
+                        <a class="carousel-control-next" href="#home-carousel" role="button" data-bs-slide="next">
                             <span class="carousel-control-next-icon" aria-hidden="true"></span>
                             <span class="visually-hidden">Next</span>
                         </a>
