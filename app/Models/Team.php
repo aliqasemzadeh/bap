3e60515717d215d8c38e84e0c87cea7f088d433a
Ali Ghasemzadeh <alighasemzadeh.dev@gmail.com>
2022-11-05 22:12:16 +0330

    Team user display.

--- app/Models/Team.php
+++ app/Models/Team.php
@@ -13,4 +13,9 @@ class Team extends \Laravel\Jetstream\Team
     {
         return $this->belongsTo(User::class);
     }
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class, 'team_user', 'user_id');
+    }
 }
--- resources/views/livewire/admin/user/team/users.blade.php
+++ resources/views/livewire/admin/user/team/users.blade.php
@@ -1,3 +1,22 @@
-<div>
-    {{-- To attain knowledge, add things every day; To attain wisdom, subtract things every day. --}}
+<div class="modal-dialog modal-xl">
+    <div class="modal-content">
+        <div class="modal-header">
+            <h5 class="modal-title">{{ __('bap.users') }}</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="{{ __('bap.close') }}"></button>
+        </div>
+        <div class="modal-body">
+            <div class="row">
+                <div class="col">
+                    <ul class="list-group">
+                        @foreach($team->users as $user)
+                            <li class="list-group-item d-flex justify-content-between align-items-center">
+                                {{ $user->name }} - {{ $user->email }}
+                            </li>
+                        @endforeach
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
+
