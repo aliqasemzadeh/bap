0af8781116e6d909981597f45934749415155db7
Ali Ghasemzadeh <alighasemzadeh.dev@gmail.com>
2022-11-16 05:57:30 +0330

    Add Mobile verify system

--- app/Http/Livewire/Panel/User/Mobile.php
+++ app/Http/Livewire/Panel/User/Mobile.php
@@ -6,6 +6,18 @@ use Livewire\Component;
 
 class Mobile extends Component
 {
+    public $mobile;
+
+    public function mount()
+    {
+
+    }
+
+    public function send()
+    {
+
+    }
+
     public function render()
     {
         return view('livewire.panel.user.mobile')->layout('layouts.panel');
