6a2560949f17f1aa1cf6eabaf071437bf360e13f
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2023-07-01 02:27:17 +0330

    fix mix.

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,7 +12,7 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css')
+    .sass('resources/scss/app.scss', 'public/css')
     .copyDirectory('resources/images', 'public/images')
     .copyDirectory('resources/favicon', 'public/favicon')
     .webpackConfig(require('./webpack.config'));
