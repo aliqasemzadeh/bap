ab0c7d753ced46d341144e702feed8a6a6886bd6
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2023-07-02 16:49:04 +0330

    fix login logo display.

--- resources/views/vendor/jetstream/components/authentication-card-logo.blade.php
+++ resources/views/vendor/jetstream/components/authentication-card-logo.blade.php
@@ -1,3 +1,4 @@
 <a class="d-flex justify-content-center mb-4" href="{{ route('home') }}">
-    <img width="128" src="{{ asset('images/logo-light.svg') }}" {{ $attributes }} />
+    <img x-show="darkTheme == 'dark'" x-transition width="128" src="{{ asset('images/logo-dark.svg') }}" {{ $attributes }} />
+    <img x-show="darkTheme != 'dark'" x-transition width="128" src="{{ asset('images/logo-light.svg') }}" {{ $attributes }} />
 </a>
