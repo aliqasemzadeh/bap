1dd0e892a13237552716b88a73bdefd81e0a2f7f
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2023-07-02 18:49:12 +0330

    fix mix.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,47 +1,18 @@
-window._ = require('lodash');
+require('./bootstrap');
 
-/**
- * We'll load jQuery and the Bootstrap jQuery plugin which provides support
- * for JavaScript based Bootstrap features such as modals and tabs. This
- * code may be modified to fit the specific needs of your application.
- */
+import Alpine from 'alpinejs';
+import Persist from '@alpinejs/persist';
+import Clipboard from "@ryangjchandler/alpine-clipboard";
 
-import '@popperjs/core'
+Alpine.plugin(Clipboard.configure({
+    onCopy: () => {
+        Swal.fire('Copied.');
+    }
+}))
 
-const bootstrap = require('bootstrap')
+Alpine.plugin(Persist);
+Alpine.plugin(Clipboard);
+window.Alpine = Alpine;
 
-window.bootstrap = bootstrap;
+Alpine.start();
 
-/**
- * We'll load the axios HTTP library which allows us to easily issue requests
- * to our Laravel back-end. This library automatically handles sending the
- * CSRF token as a header based on the value of the "XSRF" token cookie.
- */
-
-window.axios = require('axios');
-
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-
-/**
- * Echo exposes an expressive API for subscribing to channels and listening
- * for events that are broadcast by Laravel. Echo and event broadcasting
- * allows your team to easily build robust real-time web applications.
- */
-
-// import Echo from 'laravel-echo';
-
-// window.Pusher = require('pusher-js');
-
-// window.Echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: process.env.MIX_PUSHER_APP_KEY,
-//     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-//     forceTLS: true
-// });
-
-window.Chart = require('chart.js');
-
-window.Swal = require('sweetalert2');
-
-
-require('../../vendor/aliqasemzadeh/livewire-bootstrap-modal/resources/js/modals');
