4b520f201690abcda66ed07a90db18bb89c401c0
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2023-07-06 19:34:22 +0330

    make mobile filling.

--- resources/views/livewire/panel/user/mobile.blade.php
+++ resources/views/livewire/panel/user/mobile.blade.php
@@ -1,3 +1,33 @@
 <div>
-    {{-- To attain knowledge, add things every day; To attain wisdom, subtract things every day. --}}
+    <x-slot name="title">
+        {{ __('bap.mobile') }}
+    </x-slot>
+
+    <div class="row row-cards">
+        <div class="col-md-6 col-xl-12">
+            <form  wire:submit.prevent="verify_request" class="card">
+                <div class="card-header">
+                    <h4 class="card-title">{{ __('bap.mobile') }}</h4>
+                </div>
+                <div class="card-body">
+
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="mb-3">
+                                <label class="form-label">{{ __('bap.mobile') }}</label>
+                                <input type="text" wire:model="mobile" class="form-control @error('mobile') is-invalid @enderror" name="mobile">
+                                @error('mobile')
+                                <div class="invalid-feedback">{{ $message }}</div>
+                                @enderror
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="card-footer">
+                    <button type="submit" class="btn btn-primary me-auto">{{ __('bap.save') }}</button>
+                </div>
+            </form>
+        </div>
+    </div>
 </div>
