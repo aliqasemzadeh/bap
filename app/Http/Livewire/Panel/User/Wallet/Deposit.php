5ca6964493dc9c44a8c9b3b9cf61c487d3da9bdb
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2023-07-07 14:31:07 +0330

    wallet for users.

---
+++ app/Http/Livewire/Panel/User/Wallet/Deposit.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Panel\User\Wallet;
+
+use Livewire\Component;
+
+class Deposit extends Component
+{
+    public function render()
+    {
+        return view('livewire.panel.user.wallet.deposit');
+    }
+}
