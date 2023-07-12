bc25e071ac87b5e7b88a56516627ae471ba1c720
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2023-07-12 12:56:13 +0330

    fix config

--- resources/views/livewire/panel/wallet/index.blade.php
+++ resources/views/livewire/panel/wallet/index.blade.php
@@ -167,7 +167,7 @@
                                         <path d="M12 4l0 12"></path>
                                     </svg>
                                 </button>
-                                @if(__('wallet.'.$wallet->symbol.'.stack'))
+                                @if(config('wallet.'.$wallet->symbol.'.stack'))
                                 <button onclick="Livewire.emit('showModal', 'panel.wallet.stack')" class="btn btn-sm btn-primary d-none d-sm-inline-block">
                                     <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-stackoverflow" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                                         <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
