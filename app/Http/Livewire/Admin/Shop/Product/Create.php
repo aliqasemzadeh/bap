c3b999e11ad29b42222526d45835ee9a53b59d0f
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2023-07-16 11:18:16 +0330

    Add LiveWire Component to admins.

---
+++ app/Http/Livewire/Admin/Shop/Product/Create.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Admin\Shop\Product;
+
+use Livewire\Component;
+
+class Create extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.shop.product.create');
+    }
+}
---
+++ app/Http/Livewire/Admin/Shop/Product/Index.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Admin\Shop\Product;
+
+use Livewire\Component;
+
+class Index extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.shop.product.index');
+    }
+}
