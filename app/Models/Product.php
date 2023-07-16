9d56e98dfee6893663e1b75eec24065fdfb23068
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2023-07-16 10:48:06 +0330

    add models for

---
+++ app/Models/Product.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Product extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+}
