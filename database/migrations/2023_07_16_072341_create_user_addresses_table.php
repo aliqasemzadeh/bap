2aeca28d0c915a6142bbea51d43a5d017539ce37
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2023-07-16 10:57:01 +0330

    add address book for users.

---
+++ database/migrations/2023_07_16_072341_create_user_addresses_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_addresses', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->bigInteger('user_id')->index();
+            $table->longText('country_id')->index();
+            $table->longText('province_id')->index();
+            $table->longText('city_id')->index();
+            $table->longText('address');
+            $table->string('zipcode');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_addresses');
+    }
+};
