6a7e31d909ab62fc3fa7b7fbe639b7b55ee99075
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2023-07-16 07:27:44 +0330

    fix colors.

--- resources/views/livewire/admin/user/wallet/index.blade.php
+++ resources/views/livewire/admin/user/wallet/index.blade.php
@@ -43,7 +43,7 @@
                                             </svg>
                                             <span class="d-none d-sm-inline-block">{{ __('bap.export') }}</span>
                                         </button>
-                                        <button  wire:click="updateBalance({{ $wallet->id }})" class="btn btn-info btn-sm">
+                                        <button  wire:click="updateBalance({{ $wallet->id }})" class="btn btn-cyan btn-sm">
                                             <!-- Download SVG icon from http://tabler-icons.io/i/rotate -->
                                             <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M19.95 11a8 8 0 1 0 -.5 4m.5 5v-5h-5" /></svg>
                                             <span class="d-none d-sm-inline-block">{{ __('bap.update_wallet_balance') }}</span>
