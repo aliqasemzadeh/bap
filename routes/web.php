d30fc4ce6d8a35dc12f31006654b866817af0f19
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2023-07-16 12:30:43 +0330

    add routes for admin of shop.

--- routes/web.php
+++ routes/web.php
@@ -56,6 +56,9 @@ Route::group(['prefix' => LaravelLocalization::setLocale(), 'middleware' => ['re
             Route::get('/setting/category/index', \App\Http\Livewire\Admin\Setting\Category\Index::class)->name('admin.setting.category.index');
             Route::get('/setting/manage/index', \App\Http\Livewire\Admin\Setting\Manage\Index::class)->name('admin.setting.manage.index');
 
+            Route::get('/shop/product/index', \App\Http\Livewire\Admin\Shop\Product\Index::class)->name('admin.shop.product.index');
+            Route::get('/shop/order/index', \App\Http\Livewire\Admin\Shop\Order\Index::class)->name('admin.shop.order.index');
+
             Route::get('/content/article/index', \App\Http\Livewire\Admin\Content\Article\Index::class)->name('admin.content.article.index');
             Route::get('/content/faq/index', \App\Http\Livewire\Admin\Content\FAQ\Index::class)->name('admin.content.faq.index');
             Route::get('/content/carousel/index', \App\Http\Livewire\Admin\Content\Carousel\Index::class)->name('admin.content.carousel.index');
