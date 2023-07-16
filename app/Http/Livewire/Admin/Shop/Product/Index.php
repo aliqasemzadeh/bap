c93d90116709a3bcaf383166a768285b494204b5
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2023-07-16 13:58:30 +0330

    filter for products.

--- app/Http/Livewire/Admin/Shop/Product/Index.php
+++ app/Http/Livewire/Admin/Shop/Product/Index.php
@@ -2,12 +2,19 @@
 
 namespace App\Http\Livewire\Admin\Shop\Product;
 
+use App\Models\Product;
+use App\Models\User;
 use Livewire\Component;
 
 class Index extends Component
 {
     public function render()
     {
-        return view('livewire.admin.shop.product.index');
+        if(!auth()->user()->can('admin_user_index')) {
+            return abort(403);
+        }
+        $users = Product::filter(['search' => $this->search])->orderBy($this->sortColumn, $this->sortDirection)->paginate($this->perPage);
+
+        return view('livewire.admin.shop.product.index', compact('users'))->layout('layouts.admin');
     }
 }
--- database/migrations/2023_07_16_071405_create_products_table.php
+++ database/migrations/2023_07_16_071405_create_products_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('title');
+            $table->string('slug');
             $table->string('image');
             $table->double('current_price');
             $table->bigInteger('user_id')->index();
