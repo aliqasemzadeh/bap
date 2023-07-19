22fedd7ad322ffd4debd2a3e865aa61143e4fbc3
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2023-07-19 11:10:00 +0330

    product card.

--- resources/views/livewire/app/shop/product/index.blade.php
+++ resources/views/livewire/app/shop/product/index.blade.php
@@ -1,3 +1,19 @@
 <div>
-    {{-- Do your work, then step back. --}}
+    <x-slot name="title">
+        {{ __('bap.shop') }}
+    </x-slot>
+
+
+    @foreach($products as $product)
+    <div class="card">
+        <!-- Photo -->
+        <div class="img-responsive img-responsive-21x9 card-img-top" style="background-image: url(./static/photos/home-office-desk-with-macbook-iphone-calendar-watch-and-organizer.jpg)"></div>
+        <div class="card-body">
+            <h3 class="card-title">Card with top image</h3>
+            <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam deleniti fugit incidunt, iste, itaque minima
+                neque pariatur perferendis sed suscipit velit vitae voluptatem.</p>
+        </div>
+    </div>
+    @endforeach
+
 </div>
