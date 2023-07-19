400ecf6d1224c7b4c44ef3e68255887fcd4942a8
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2023-07-19 10:51:48 +0330

    add shop to products.

---
+++ app/Http/Livewire/App/Shop/Product/View.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\App\Shop\Product;
+
+use Livewire\Component;
+
+class View extends Component
+{
+    public function render()
+    {
+        return view('livewire.app.shop.product.view');
+    }
+}
