9900fee49c4ca0d7241e6aeeea55a0d57b3cfab9
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2023-07-21 13:34:42 +0330

    shopping cart checkout.

--- app/Http/Livewire/App/Shop/Cart/Index.php
+++ app/Http/Livewire/App/Shop/Cart/Index.php
@@ -2,10 +2,18 @@
 
 namespace App\Http\Livewire\App\Shop\Cart;
 
+use Jantinnerezo\LivewireAlert\LivewireAlert;
 use Livewire\Component;
 
 class Index extends Component
 {
+    use LivewireAlert;
+    protected $listeners = ['updateCartIndex' => 'render'];
+    public function removeItem($itemId)
+    {
+        \Cart::remove($itemId);
+        $this->emit('updateCartIndex');
+    }
     public function render()
     {
         return view('livewire.app.shop.cart.index');
--- resources/views/livewire/app/shop/cart/index.blade.php
+++ resources/views/livewire/app/shop/cart/index.blade.php
@@ -3,5 +3,44 @@
         {{ __('bap.cart') }}
     </x-slot>
 
-
+    <div class="row row-cards">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="card-title">{{ __('bap.cart') }}</h3>
+                </div>
+                <div class="list-group list-group-flush list-group-hoverable">
+                    @foreach(\Cart::getContent() as $item)
+                    <div class="list-group-item">
+                        <div class="row align-items-center">
+                            <div class="col-auto">
+                                <a href="{{ route('shop.product.view', [$item->id]) }}">
+                                    <span class="avatar" style="background-image: url( {{ $item->attributes->icon }} )"></span>
+                                </a>
+                            </div>
+                            <div class="col text-truncate">
+                                <a href="#" class="text-reset d-block">{{ $item->name }}</a>
+                                <div class="d-block text-muted text-truncate mt-n1">
+                                    {{ $item->quantity }} X {{ $item->price }} = {{ $item->getPriceSum() }}
+                                </div>
+                            </div>
+                            <div class="col-auto">
+                                <button wire:click="removeItem({{$item->id}})" class="list-group-item-actions"><!-- Download SVG icon from http://tabler-icons.io/i/star -->
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-trash" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                        <path d="M4 7l16 0"></path>
+                                        <path d="M10 11l0 6"></path>
+                                        <path d="M14 11l0 6"></path>
+                                        <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12"></path>
+                                        <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3"></path>
+                                    </svg>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    @endforeach
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
