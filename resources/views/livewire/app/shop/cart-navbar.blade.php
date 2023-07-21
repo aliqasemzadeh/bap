ff216ea047d429ac4ec78a5a50336ce58614233a
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2023-07-21 13:06:35 +0330

    shopping cart badge.

--- resources/views/livewire/app/shop/cart-navbar.blade.php
+++ resources/views/livewire/app/shop/cart-navbar.blade.php
@@ -10,7 +10,9 @@
             <path d="M17 17h-11v-14h-2"></path>
             <path d="M6 5l14 1l-1 7h-13"></path>
         </svg>
-        <span class="badge bg-red"></span>
+        @if(\Cart::getContent()->count())
+            <span class="badge bg-red"></span>
+        @endif
     </a>
     <div class="dropdown-menu dropdown-menu-arrow dropdown-menu-end dropdown-menu-card">
         <div class="card">
