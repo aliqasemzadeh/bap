749dfcc046f3a6929262d02dcf490e79047dc8e6
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2023-07-22 09:30:31 +0330

    deposits & withdraws

--- resources/views/livewire/admin/payment/deposit/index.blade.php
+++ resources/views/livewire/admin/payment/deposit/index.blade.php
@@ -1,3 +1,12 @@
 <div>
-    {{-- Knowing others is intelligence; knowing yourself is true wisdom. --}}
+    <x-slot name="title">
+        {{ __('bap.deposits') }}
+    </x-slot>
+
+    <x-slot name="breadcrumb">
+        <ol class="breadcrumb breadcrumb-arrows" aria-label="breadcrumbs">
+            <li class="breadcrumb-item"><a href="{{ route('admin.dashboard.index') }}">{{ __('bap.dashboard') }}</a></li>
+            <li class="breadcrumb-item active" aria-current="page"><a href="{{ route('admin.payment.depsoit.index') }}">{{ __('bap.deposits') }}</a></li>
+        </ol>
+    </x-slot>
 </div>
--- resources/views/livewire/admin/payment/withdraw/index.blade.php
+++ resources/views/livewire/admin/payment/withdraw/index.blade.php
@@ -1,3 +1,12 @@
 <div>
-    {{-- If you look to others for fulfillment, you will never truly be fulfilled. --}}
+    <x-slot name="title">
+        {{ __('bap.withdraws') }}
+    </x-slot>
+
+    <x-slot name="breadcrumb">
+        <ol class="breadcrumb breadcrumb-arrows" aria-label="breadcrumbs">
+            <li class="breadcrumb-item"><a href="{{ route('admin.dashboard.index') }}">{{ __('bap.dashboard') }}</a></li>
+            <li class="breadcrumb-item active" aria-current="page"><a href="{{ route('admin.payment.withdraw.index') }}">{{ __('bap.withdraws') }}</a></li>
+        </ol>
+    </x-slot>
 </div>
