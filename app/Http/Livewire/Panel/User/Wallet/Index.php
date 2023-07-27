0e5c61ab087d51e259313001fad24f7919bc61d1
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2023-07-27 08:41:01 +0330

    add wallet linke.

--- app/Http/Livewire/Panel/User/Wallet/Index.php
+++ app/Http/Livewire/Panel/User/Wallet/Index.php
@@ -8,6 +8,6 @@ class Index extends Component
 {
     public function render()
     {
-        return view('livewire.panel.user.wallet.index');
+        return view('livewire.panel.user.wallet.index')->layout('layouts.panel');
     }
 }
--- resources/views/livewire/panel/user/wallet/index.blade.php
+++ resources/views/livewire/panel/user/wallet/index.blade.php
@@ -3,5 +3,17 @@
         {{ __('bap.user_wallets') }}
     </x-slot>
 
-
+    <x-slot name="actions">
+            <div class="col-auto ms-auto d-print-none">
+                <div class="btn-list">
+                    <button onclick="Livewire.emit('showModal', 'panel.user.wallet.create')" class="btn btn-primary d-none d-sm-inline-block">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg>
+                        {{ __('bap.create_user_wallet') }}
+                    </button>
+                    <button onclick="Livewire.emit('showModal', 'admin.panel.user.wallet.create')" class="btn btn-primary d-sm-none btn-icon" aria-label="Create new report">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg>
+                    </button>
+                </div>
+            </div>
+    </x-slot>
 </div>
