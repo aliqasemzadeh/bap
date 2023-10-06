21bee29c723829d34626abb613b8bbd2e5a9e6c0
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2023-10-06 15:46:41 +0330

    update event to livewire 3.

--- resources/views/layouts/global/foot-js.blade.php
+++ resources/views/layouts/global/foot-js.blade.php
@@ -4,4 +4,12 @@
 <x-livewire-alert::scripts />
 <script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
 
+
+<script>
+    document.addEventListener('livewire:initialized', () => {
+            alert('showModal');
+    });
+</script>
+
+
 @stack('scripts')
