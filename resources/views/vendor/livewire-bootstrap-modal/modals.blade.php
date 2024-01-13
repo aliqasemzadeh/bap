e8d747e141e63c808981e7227657512340eb1827
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2024-01-13 04:54:52 +0330

    try to fix modals.

--- resources/views/vendor/livewire-bootstrap-modal/modals.blade.php
+++ resources/views/vendor/livewire-bootstrap-modal/modals.blade.php
@@ -1,10 +1,9 @@
 <div id="livewire-bootstrap-modal" tabindex="-1"
-    data-bs-backdrop="static" data-bs-keyboard="false"
-    wire:ignore.self class="modal modal-blur fade">
+     data-bs-backdrop="static" data-bs-keyboard="false"
+     wire:ignore.self class="modal fade">
 
     @if($alias)
         @livewire($alias, $params, key($alias))
     @endif
 
 </div>
-
