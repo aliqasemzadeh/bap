7f22d9eed52bc3eabba52e921024f88f73c1623a
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2024-01-13 05:00:24 +0330

    add change language.

--- resources/views/layouts/global/user-navbar.blade.php
+++ resources/views/layouts/global/user-navbar.blade.php
@@ -13,6 +13,17 @@
             <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
             <path d="M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1 -8.313 -12.454z"/>
         </svg>
+
+    </div>
+    <div class="nav-item dropdown me-3">
+        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-language" width="24" height=24" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+            <path d="M4 5h7" />
+            <path d="M9 3v2c0 4.418 -2.239 8 -5 8" />
+            <path d="M5 9c0 2.144 2.952 3.908 6.7 4" />
+            <path d="M12 20l4 -9l4 9" />
+            <path d="M19.1 18h-6.2" />
+        </svg>
     </div>
     @auth
         @if(config('modules.notification'))
