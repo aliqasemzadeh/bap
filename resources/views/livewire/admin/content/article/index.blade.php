24a834bfdd969333553dfbf3b5defc65d5827775
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2024-01-23 16:28:52 +0330

    fix words.

--- resources/views/livewire/admin/content/article/index.blade.php
+++ resources/views/livewire/admin/content/article/index.blade.php
@@ -10,7 +10,7 @@
                         <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg>
                         {{ __('bap.create_article') }}
                     </button>
-                    <button wire:click="openCreate" class="btn btn-primary d-sm-none btn-icon" aria-label="Create new report">
+                    <button wire:click="openCreate" class="btn btn-primary d-sm-none btn-icon" aria-label="{{ __('bap.create_article') }}">
                         <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg>
                     </button>
                 </div>
