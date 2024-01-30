33c0d66b685e647aafb762984021621f6a5e76ad
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2024-01-30 10:08:05 +0330

    fix modal dispatching

--- resources/views/livewire/app/article/index.blade.php
+++ resources/views/livewire/app/article/index.blade.php
@@ -12,7 +12,7 @@
 
             @can('admin_article_create')
             <div class="empty-action">
-                <button onclick="Livewire.emit('showModal', 'admin.content.article.create')" class="btn btn-primary">
+                <button wire:click="$dispatch('showModal', {data:{alias:'admin.content.article.create'}})" class="btn btn-primary">
                     <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg>
                     {{ __('bap.create_article') }}
                 </button>
