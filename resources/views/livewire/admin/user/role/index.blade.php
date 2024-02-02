00f2194d63e654b4ba6bd7092fd98d7230b19e6f
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2024-02-02 09:04:54 +0330

    fix role editing.

--- resources/views/livewire/admin/user/role/index.blade.php
+++ resources/views/livewire/admin/user/role/index.blade.php
@@ -104,7 +104,7 @@
                                     <path d="M21 21v-2a4 4 0 0 0 -3 -3.85"></path>
                                 </svg>
                             </button>
-                            <button wire:click="$dispatch('showModal', 'admin.user.role.edit', '{{ json_encode($role->id) }}')" class="btn btn-primary btn-icon btn-sm">
+                            <button wire:click="$dispatch('showModal', {data:{alias:'admin.user.role.edit', role_id:'{{ $role->id }}'}})" class="btn btn-primary btn-icon btn-sm">
                                 <!-- Download SVG icon from http://tabler-icons.io/i/edit -->
                                 <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M9 7h-3a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-3" /><path d="M9 15h3l8.5 -8.5a1.5 1.5 0 0 0 -3 -3l-8.5 8.5v3" /><line x1="16" y1="5" x2="19" y2="8" /></svg>
                             </button>
