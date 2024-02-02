16dc59b5e47f84db5813eb7a4b040cda401a9595
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2024-02-02 07:59:15 +0330

    fix dashboard layout.

--- resources/views/livewire/admin/dashboard/index.blade.php
+++ resources/views/livewire/admin/dashboard/index.blade.php
@@ -1,13 +1,21 @@
-<div>
-    <x-slot name="title">
-        {{ __('bap.dashboard') }}
-    </x-slot>
-    <x-slot name="breadcrumb">
-        <ol class="breadcrumb breadcrumb-arrows" aria-label="breadcrumbs">
-            <li class="breadcrumb-item active" aria-current="page"><a href="{{ route('admin.dashboard.index') }}">{{ __('bap.dashboard') }}</a></li>
-        </ol>
-    </x-slot>
-    <div class="row row-cards">
+<x-slot name="title">
+    {{ __('bap.dashboard') }}
+</x-slot>
+<main class="{{ config('bap.container', 'container-fluid') }}">
+    <div class="page-header d-print-none">
+        <div class="row align-items-center">
+            <div class="col">
+                <h2 class="page-title">
+                    {{ __('bap.dashboard') }}
+                </h2>
+                <ol class="breadcrumb breadcrumb-arrows" aria-label="breadcrumbs">
+                    <li class="breadcrumb-item active" aria-current="page"><a href="{{ route('admin.dashboard.index') }}">{{ __('bap.dashboard') }}</a></li>
+                </ol>
+            </div>
+        </div>
+    </div>
+    <div class="page-body">
+        <div class="row row-cards">
         <div class="col-md-4">
             <div class="card card-sm">
                 <div class="card-body">
@@ -73,4 +81,5 @@
             </div>
         </div>
     </div>
-</div>
+    </div>
+</main>
